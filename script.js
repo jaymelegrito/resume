7aab3e45aaf6b4dbf40961f4b98432a39526a3ab
jaymelegrito <melegrito.john@gmail.com>
2019-10-08 19:02:50 +0800

    revised script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementsByTagName("nav");
-var hdr = $('header').height();
+var header = document.getElementsByTagName("header");
+var ht = header.height;
 
 function myFunction() {
-    if (window.pageYOffset > hdr) {
+    if (window.pageYOffset > ht) {
         navbar.classList.add("nav-scrolled")
     }
     else {
         navbar.classList.remove("nav-scrolled");
-
     }
 }
